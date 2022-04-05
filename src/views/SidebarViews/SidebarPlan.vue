6187a3c416bd95d319e2625a03e4bea05d3ffaf8
deicidem <kronosdidi@gmail.com>
2022-04-05 11:51:01 +0300

    add button and input component

--- src/views/SidebarViews/SidebarPlan.vue
+++ src/views/SidebarViews/SidebarPlan.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
     <h2>Plan</h2>
+    <app-button>Использовать карту</app-button>
+    <app-button type="white">Использовать карту</app-button>
+    <app-button type="white-g">Использовать карту</app-button>
+    <app-button type="white-r">Использовать карту</app-button>
+    <app-button type="red">Использовать карту</app-button>
+    <app-input label="Фамилия" v-model="val"></app-input>
+    {{val}}
   </div>
 </template>
 
 <script>
+import AppButton from "@/components/controls/AppButton.vue"
+import AppInput from "@/components/controls/AppInput.vue"
 export default {
-  
+  components: {
+    AppButton,
+    AppInput
+  },
+  data() {
+    return {
+      val: "инпут"
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+  div {
+    padding: 20px;
+  }
+</style>

940603f35ad4faf4ffd0ff709d392efcd617f81d
deicidem <kronosdidi@gmail.com>
2022-04-05 10:25:37 +0300

    start remaking

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,6 @@
 <template>
   <div class="wrapper" @click="closeUser($event)">
-    <header>
-      <div class="project__name">
-        <div class="project__wrapper">
-          <div class="project__title">
-            Автоматизация процессов оценки качества данных ДЗЗ
-          </div>
-        </div>
-      </div>
-      <div class="header__wrapper">
-        <div class="links__wrapper">
-          <div class="header__item logo">
-            <router-link to="/">Home</router-link>
-            <!-- <img src="@/assets/logo.png" /> -->
-          </div>
-          <div class="header__item">
-            <router-link to="/about">About</router-link>
-            <!-- <a href="./pages/manual.html">Руководства пользователя</a> -->
-          </div>
-          <div class="header__item">
-            <!-- <a href="./pages/help.html">События</a> -->
-          </div>
-          <div class="header__item">
-            <!-- <a href="./pages/help.html">Помощь</a> -->
-          </div>
-        </div>
-        <div class="header__user">
-          <div class="user-box" v-show="showUser">
-            <div class="user-box-auth">Авторизация</div>
-            <div class="user-box-line"></div>
-            <div class="user-box-form">
-              <form>
-                <div class="user-box-input">
-                  <input type="login" placeholder="логин" />
-                  <i class="fa fa-sign-in" aria-hidden="true"></i>
-                </div>
-                <div class="user-box-input">
-                  <input type="password" placeholder="пароль" />
-                  <i class="fa fa-key" aria-hidden="true"></i>
-                </div>
-                <a href="./index.html" class="user-box-button"><p>Войти</p></a>
-                <a
-                  href="./pages/registration.html"
-                  class="user-box-button-registr"
-                  >Регистрация</a
-                >
-              </form>
-            </div>
-          </div>
-
-          <div class="search-box" v-show="showSearch">
-            <div class="search-box-title">ВВедите запрос</div>
-            <div class="user-box-line"></div>
-            <div class="user-box-input">
-              <input type="text" placeholder="поиск" />
-              <i class="fa fa-search" aria-hidden="true"></i>
-            </div>
-          </div>
-          <div class="user__item">Добро пожаловать!</div>
-          <div class="user__item">
-            <i
-              @click="showSearch = !showSearch"
-              id="search-box__click"
-              class="fa fa-search search-box__click"
-              aria-hidden="true"
-            ></i>
-            <i
-              @click="showUser = !showUser"
-              id="user-box__click"
-              class="fa fa-user user-box__click"
-              aria-hidden="true"
-            ></i>
-          </div>
-        </div>
-      </div>
-    </header>
+    <app-header></app-header>
 
     <div class="content">
       <router-view></router-view>
@@ -83,7 +9,12 @@
 </template>
 
 <script>
+import AppHeader from "@/components/AppHeader.vue"
+
 export default {
+  components: {
+    AppHeader
+  },
   data() {
     return {
       showUser: false,
@@ -119,6 +50,8 @@ export default {
 </script>
 
 <style lang="scss">
+
+
 * {
   margin: 0;
   padding: 0;
@@ -129,7 +62,7 @@ html,
 body {
   color: #868686;
   font-weight: 400;
-  font-size: 14px;
+  font-size: 16px;
   line-height: 1.5;
   font-family: Helvetica, sans-serif;
   min-width: 1024px;
@@ -159,223 +92,14 @@ img {
 }
 
 .wrapper {
-  display: flex;
   min-height: 100vh;
-  flex-direction: column;
+  background: #DFDFDF;
 }
 
-.content {
-  flex: 1 1 auto;
-  display: flex;
-}
 
-.container {
-  max-width: 1920px;
-  width: 100%;
-  padding: 0 15px;
-  margin: 0 auto;
-  box-sizing: border-box;
-}
-
-.authorize {
-  background: url("@/assets/authorize__background.png");
-  width: 100%;
-  height: 100vh;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.registr {
-  background: url("@/assets/authorize__background.png");
-  width: 100%;
-  height: 100vh;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.language {
-  color: #ffffff;
-  font-size: 24px;
-  text-align: right;
-  margin-top: 50px;
-  font-weight: 400;
-}
-
-.form {
-  position: absolute;
-  top: 35%;
-  left: 40%;
-}
-
-.title {
-  font-weight: 400;
-  font-size: 30px;
-  text-align: center;
-  color: #ffffff;
-}
-
-.input {
-  width: 100%;
-  box-sizing: border-box;
-  border: 4px solid transparent;
-  outline: none;
-  position: relative;
-  margin-top: 30px;
-  transition: 0.5s;
-  border-radius: 4px;
-}
-
-.input input {
-  padding: 20px 0;
-  padding-left: 50px;
-  outline: none;
-  width: 400px;
-  font-size: 16px;
-  border-radius: 4px;
-  border: 0;
-}
-
-.input:focus-within {
-  border: 4px solid #618580;
-  box-shadow: 0 0 16px 0 #618580;
-  -webkit-box-shadow: 0 0 16px 0 #618580;
-  -moz-box-shadow: 0 0 16px 0 #618580;
-}
-
-.input:focus-within .fa {
-  color: #618580;
-}
-
-.input i {
-  position: absolute;
-  top: 18px;
-  left: 10px;
-  font-size: 24px;
-}
-
-.button {
-  background: rgb(97, 133, 128, 0.6);
-  border: 3px solid #618580;
-  margin-top: 30px;
-  border-radius: 35px;
-  font-size: 22px;
-  color: #ffffff;
-  display: block;
-  text-align: center;
-  line-height: 59px;
-  font-weight: 400;
-}
-
-.button-registr {
-  border: 3px solid #618580;
-  margin-top: 30px;
-  border-radius: 35px;
-  font-size: 22px;
-  color: #ffffff;
-  display: block;
-  text-align: center;
-  line-height: 59px;
-  font-weight: 400;
-}
-
-.button:hover {
-  box-shadow: 0 0 16px 0 #618580;
-  -webkit-box-shadow: 0 0 16px 0 #618580;
-  -moz-box-shadow: 0 0 16px 0 #618580;
-  transition: all 0.4s ease-out;
-}
-
-.button-registr:hover {
-  box-shadow: 0 0 16px 0 #618580;
-  -webkit-box-shadow: 0 0 16px 0 #618580;
-  -moz-box-shadow: 0 0 16px 0 #618580;
-  transition: all 0.4s ease-out;
-}
-
-.registr-form__sign-in {
-  text-align: center;
-  margin-top: 20px;
-  color: #ffffff;
-  display: block;
-  font-weight: 400;
-}
-
-.registr-form__sign-in:hover {
-  color: #618580;
-  transition: all 0.4s ease-out;
-}
 
 /* _____________________index.html__________________ */
 
-header {
-  z-index: 1;
-  box-shadow: 0 0 16px 0 #384342;
-  -webkit-box-shadow: 0 0 16px 0 #384342;
-  -moz-box-shadow: 0 0 16px 0 #384342;
-}
-
-.project__name {
-  background-color: #618580;
-}
-
-.project__wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.project__title {
-  width: 50%;
-  padding: 16px;
-  color: #fff;
-  line-height: 150%;
-  font-size: 20px;
-  text-align: center;
-  font-weight: 400;
-}
-
-.header__wrapper {
-  display: flex;
-  justify-content: space-between;
-  align-items: stretch;
-  padding: 0px 20px;
-  background-color: #384342;
-  border-top: 1px solid #fff;
-  
-
-}
-
-.links__wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-}
-
-.header__user {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  position: relative;
-}
-
-.user__item i:hover {
-  color: #618580;
-  text-shadow: 0 0 16px 0 #618580;
-  -webkit-text-shadow: 0 0 16px 0 #618580;
-  -moz-text-shadow: 0 0 16px 0 #618580;
-}
-
-.user__item {
-  text-align: right;
-  color: #fff;
-  font-weight: 400;
-  font-size: 18px;
-}
-
-.user__item i {
-  margin: 10px;
-  font-size: 24px;
-}
 
 /* ______________________user-box____________________ */
 
@@ -526,28 +250,7 @@ header {
   transition: all 0.4s ease-out;
 }
 
-header a {
-  text-decoration: none;
-  color: #fff;
-  font-weight: 400;
-  font-size: 18px;
-  padding: 6px 15px;
-  box-sizing: border-box;
-}
 
-header a:hover {
-  // background: #618580;
-  border-radius: 4px;
-  transition: background-color 0.6s ease-out;
-}
-
-.header__item {
-  margin-right: 40px;
-}
-
-.logo {
-  max-width: 120px;
-}
 
 content {
   width: 100%;
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,163 @@
+<template>
+  <header class="header">
+    <h1 class="title">Автоматизация процессов оценки качества данных ДЗЗ</h1>
+    <div class="header-items">
+      <nav class="header-nav">
+        <ul>
+          <li class="header-nav__item">
+            <router-link to="/">Главная</router-link>
+          </li>
+          <li class="header-nav__item">
+            <router-link to="/about">Руководства пользователя</router-link>
+          </li>
+          <li class="header-nav__item">
+            <router-link to="/about">Помощь</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="header-menu">
+        <!-- <div class="user-box">
+            <div class="user-box-auth">Авторизация</div>
+            <div class="user-box-line"></div>
+            <div class="user-box-form">
+              <form>
+                <div class="user-box-input">
+                  <input type="login" placeholder="логин" />
+                  <i class="fa fa-sign-in" aria-hidden="true"></i>
+                </div>
+                <div class="user-box-input">
+                  <input type="password" placeholder="пароль" />
+                  <i class="fa fa-key" aria-hidden="true"></i>
+                </div>
+                <a href="./index.html" class="user-box-button"><p>Войти</p></a>
+                <a
+                  href="./pages/registration.html"
+                  class="user-box-button-registr"
+                  >Регистрация</a
+                >
+              </form>
+            </div>
+          </div>
+
+          <div class="search-box" >
+            <div class="search-box-title">ВВедите запрос</div>
+            <div class="user-box-line"></div>
+            <div class="user-box-input">
+              <input type="text" placeholder="поиск" />
+              <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+          </div> -->
+        <div class="header-menu__text">Добро пожаловать!</div>
+        <div class="header-menu__buttons">
+          <button class="button button-o header-menu__button">
+            <i class="fa fa-search" aria-hidden="true"></i>
+          </button>
+          <button class="button button-o header-menu__button">
+            <i class="fa fa-user" aria-hidden="true"></i>
+          </button>
+          
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  background: $gradient;
+  border-radius: 5px;
+  border: none;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
+}
+
+.button-o {
+  background: $gradient-w;
+  color: $color-main-dark;
+}
+.title {
+  position: relative;
+  font-size: 1.5rem;
+  font-weight: 400;
+  padding: 10px;
+  text-align: center;
+  background: $gradient;
+  box-shadow: $shadow-big;
+  color: #fff;
+}
+
+.header-user {
+  display: none;
+}
+
+.user-box {
+  display: none;
+}
+
+.search-box {
+  display: none;
+}
+.header-items {
+  display: flex;
+}
+.header {
+  z-index: 1;
+  position: relative;
+  &-items {
+    background: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 30px 100px;
+    box-shadow: $shadow-big;
+  }
+
+  &-nav {
+    ul {
+      display: flex;
+      align-items: center;
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      li {
+        margin-right: 50px;
+        a {
+          color: #000;
+          font-size: 20px;
+        }
+      }
+    }
+  }
+
+  &-menu {
+    display: flex;
+    align-items: center;
+    &__text {
+      margin-right: 50px;
+      font-size: 1.25rem;
+      color: #000;
+    }
+    &__buttons {
+      display: flex;
+      align-items: center;
+    }
+    &__button {
+      margin-left: 20px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 40px;
+      height: 40px;
+      font-size: 1.5rem;
+      margin-top: 0;
+      // На IE 11 не работает
+      @supports (background-clip: text) {
+        i {
+          background: $gradient;
+          background-clip: text;
+          color: rgba(0, 0, 0, 0)
+        }
+      }
+    }
+  }
+}
+</style>

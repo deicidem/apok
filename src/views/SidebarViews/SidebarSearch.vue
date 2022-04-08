a0f90904dd5639fd7433258cd323c9154ba49685
Ann <ann.afanaseva777@gmail.com>
2022-04-08 17:04:21 +0300

    Fixing SidebarSearch

--- src/views/SidebarViews/SidebarSearch.vue
+++ src/views/SidebarViews/SidebarSearch.vue
@@ -1,127 +1,120 @@
 <template>
   <div class="search">
     <h2 class="sidebar-title">Поиск снимков</h2>
-
     <div class="search-wrapper">
-      <div class="search-zone">
-        <h2 class="search__title">Зона интереса</h2>
+      <vuescroll :ops="ops">
+        <div class="search-zone">
+          <h2 class="search__title">Зона интереса</h2>
 
-        <div class="search-zone__input">
-          <input type="radio" />
-          <label>Видимая область экрана</label>
+          <div class="search-zone__input">
+            <input type="radio" />
+            <label>Видимая область экрана</label>
+          </div>
+          <div class="search-zone__input">
+            <input type="radio" />
+            <label>Задать вручную</label>
+          </div>
+
+          <nav class="search-zone__nav">
+            <ul>
+              <li>
+                <a href="#">Задать полигон</a>
+              </li>
+              <li class="line"></li>
+              <li>
+                <a href="#">Вывести координаты</a>
+              </li>
+              <li class="line"></li>
+              <li>
+                <a href="#">Загрузить файл</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div class="search-zone__main">
+            <div class="search-zone__table">
+              <table>
+                <thead>
+                  <tr>
+                    <th class="col"><input type="checkbox" /></th>
+                    <th>Широта</th>
+                    <th>Долгота</th>
+                    <th class="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(coord, i) in getFormattedCoordinates" :key="i">
+                    <td class="col"><input type="checkbox" /></td>
+                    <td>{{ coord.lat }}</td>
+                    <td>{{ coord.lng }}</td>
+                    <td class="col delete" @click="deleteCoordinate(i)">
+                      <app-button class="col-item__trash" type="white-r">
+                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                      </app-button>
+                    </td>
+                  </tr>
+              </tbody>
+              </table>
+            </div>
+
+            <div class="search-zone__buttons">
+              <app-button
+                class="search-zone__button"
+                @click="setPolygonDrawable(true)"
+                >Использовать карту</app-button
+              >
+              <app-button class="search-zone__button" type="white-g"
+                >Прописать координаты</app-button
+              >
+              <app-button
+                @click="clearCoordinates()"
+                class="search-zone__button"
+                type="red"
+                >Очистить координаты</app-button
+              >
+            </div>
+          </div>
         </div>
-        <div class="search-zone__input">
-          <input type="radio" />
-          <label>Задать вручную</label>
-        </div>
 
-        <nav class="search-zone__nav">
-          <ul>
-            <li>
-              <a href="#">Задать полигон</a>
-            </li>
-            <li class="line"></li>
-            <li>
-              <a href="#">Вывести координаты</a>
-            </li>
-            <li class="line"></li>
-            <li>
-              <a href="#">Загрузить файл</a>
-            </li>
-          </ul>
-        </nav>
+        <div class="search-date">
+          <h2 class="search__title">Интервал дат съемки</h2>
+          <div class="search-date__inputs">
 
-        <div class="search-zone__main">
-          <div class="search-zone__table">
-            <table>
-              <thead>
-                <tr>
-                  <th class="col"><input type="checkbox" /></th>
-                  <th>Широта</th>
-                  <th>Долгота</th>
-                  <th class="col"></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(coord, i) in getFormattedCoordinates" :key="i">
-                  <td class="col"><input type="checkbox" /></td>
-                  <td>{{ coord.lat }}</td>
-                  <td>{{ coord.lng }}</td>
-                  <td class="col delete" @click="deleteCoordinate(i)">
-                    <app-button class="col-item__trash" type="white-r">
-                      <i class="fa fa-trash-o" aria-hidden="true"></i>
-                    </app-button>
-                  </td>
-                </tr>
-            </tbody>
-            </table>
-          </div>
 
-          <div class="search-zone__buttons">
-            <app-button
-              class="search-zone__button"
-              @click="setPolygonDrawable(true)"
-              >Использовать карту</app-button
-            >
-            <app-button class="search-zone__button" type="white-g"
-              >Прописать координаты</app-button
-            >
-            <app-button
-              @click="clearCoordinates()"
-              class="search-zone__button"
-              type="red"
-              >Очистить координаты</app-button
-            >
+            <div class="search-date__input">
+              <label class="datepicker-label" for="datepicker-start">Искать с:</label>
+              <date-picker id="datepicker-start" class="input-date" v-model="time" valueType="format">
+                <div class="datepicker__back"></div>
+              </date-picker>
+            </div>
+
+            <div class="search-date__arrow">
+              <img src="@/assets/img/arrow.png" />
+            </div>
+
+            <div class="search-date__input">
+              <label class="datepicker-label" for="datepicker-end">До:</label>
+              <date-picker id="datepicker-end" class="input-date" v-model="time" valueType="format"></date-picker>
+            </div>
+
+            <div>
+              <Select2 v-model="myValue" :options="myOptions" :settings="{ settingOption: value, settingOption: value }" @change="myChangeEvent($event)" @select="mySelectEvent($event)" />
+            </div>
+
           </div>
         </div>
-      </div>
 
+        <div class="search-cloud">
+          <h2 class="search__title">Облачность</h2>
 
-      <div class="search-date">
-        <h2 class="search__title">Интервал дат съемки</h2>
-        <div class="search-date__inputs">
-          <div class="search-date__input">
-            <i class="fa fa-calendar" aria-hidden="true"></i>
-            <input class="input-date" type="date" />
-          </div>
-
-          <div class="search-date__arrow">
-            <img src="@/assets/img/arrow.png" />
-          </div>
-
-          <div class="search-date__input">
-            <i class="fa fa-calendar" aria-hidden="true"></i>
-            <input class="input-date" type="date" />
-          </div>
-
-          <select class="search-date__select input-date">
-            <option class="search-date__option" value="value3" selected>
-              Все
-            </option>
-            <option class="search-date__option" value="value1">Январь</option>
-            <option class="search-date__option" value="value2">Февраль</option>
-            <option class="search-date__option" value="value2">Март</option>
-            <option class="search-date__option" value="value2">Апрель</option>
-            <option class="search-date__option" value="value2">Май</option>
-            <option class="search-date__option" value="value2">Июнь</option>
-            <option class="search-date__option" value="value2">Июль</option>
-            <option class="search-date__option" value="value2">Август</option>
-          </select>
+            <no-ui-slider :config="your_config" :values="your_value"/>
+            {{your_value}}
         </div>
-      </div>
 
-
-      <div class="search-cloud">
-        <h2 class="search__title">Облачность</h2>
-
-        <no-ui-slider :config="your_config" :values="your_value"/>
-      </div>
-
-      <div class="search-spacecraft">
-          <h2 class="search__title">Космический аппарат</h2>
+        <div class="search-spacecraft">
+            <h2 class="search__title">Космический аппарат</h2>
 
               <div class="accordion">
-
                 <div class="accordion-item">
                   <input class="accordion-item__input" type="checkbox" id="accordion-1">
                   <label class="accordion-item__trigger" for="accordion-1">
@@ -159,7 +152,6 @@
                     </div>
                   </div>
                 </div>
-
                 <div class="accordion-item">
                   <input class="accordion-item__input" type="checkbox" id="accordion-3">
                   <label class="accordion-item__trigger" for="accordion-3">
@@ -197,37 +189,70 @@
                     </div>
                   </div>
                 </div>
-
               </div>
         </div>
+      </vuescroll>
+      
     </div>
   </div>
 </template>
 
 <script>
-import 'vue-nouislider/dist/vue-nouislider.css'
-  // import the styling, css or scss
+import NoUiSlider from 'vue-nouislider/src/components/noUiSlider.vue';
+import 'vue-nouislider/dist/vue-nouislider.css';
+
+import vuescroll from 'vuescroll';
+import "vuescroll/dist/vuescroll.css";
+
 import { mapGetters, mapActions } from "vuex";
 import AppButton from "@/components/controls/AppButton.vue";
-import NoUiSlider from 'vue-nouislider/src/components/noUiSlider.vue';
+
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+
+import Select2 from 'v-select2-component';
+
 // import AppInput from "@/components/controls/AppInput.vue"
 export default {
   components: {
     AppButton,
-    // AppInput
-    NoUiSlider
+    vuescroll,
+    DatePicker,
+    NoUiSlider,
+    Select2
   },
   data() {
     return {
+      myOptions: ['май', 'май', 'май', 'май', 'май', 'май'],
       your_config: {
         step: 10,
         connect: true,
+        behaviour: "drag",
         range: {
           'min': 0,
           'max': 100
         }
       },
-      your_value: [0, 100]
+      ops: {
+        vuescroll: {},
+        scrollPanel: {
+        },
+        rail: {
+          background: '#000',
+          opacity: .1,
+          size: '6px',
+          specifyBorderRadius: false,
+          gutterOfEnds: null,
+          gutterOfSide: '2px',
+          keepShow: false
+        },
+        bar: {
+          onlyShowBarOnScroll: false,
+          keepShow: true,
+          background: '#000',
+        }
+      },
+      your_value: [20, 80]
     };
   },
   computed: {
@@ -241,20 +266,66 @@ export default {
       "setPolygonDrawable",
       "clearCoordinates",
     ]),
-  },
+  }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+.select2{
+  margin-left: 30px;
+  &-selection{
+    margin-left: 30px;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: $shadow-small;
+    align-items: center;
+    color: $color-main-dark;
+    height: 100%;
+    height: 30px;
+  }
+  &-container--default{
+  }
+}
+
+.vue-nouislider{
+  background: #FFF;
+  margin: 30px 0;
+  width: 450px;
+  height: 14px;
+  border-radius: 10px;
+  border: none;
+  box-shadow: $shadow-big;
+}
+
+.noUi{
+  &-connect{
+    background: $color-main-dark;
+  }
+  &-horizontal &-handle{
+    width: 24px;
+    height: 24px;
+  }
+  &-handle{
+    border-radius: 6px;
+    background: $gradient;
+    border: none;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
+    &::before{
+      background: none;
+    }
+    &::after{
+      background: none;
+    }
+  }
+}
 
 .accordion{
   padding: 10px;
-  box-shadow: $shadow-small;;
+  box-shadow: $shadow-small;
   border-radius: 10px;
   overflow: hidden;
   background: $gradient-w;
-  overflow-y: scroll;
-  max-height: 160px;
   &-item{
     position: relative;
     p{
@@ -294,22 +365,74 @@ export default {
   }
 }
 
-
-.input-date{
-  width: 160px;
-  padding: 5px 10px;
-  border: none;
-  box-shadow: $shadow-small;
-  line-height: 30px;
-  font-size: 0.875rem;
-  color: $color-main-dark;
-  background: #fff;
+.mx{
+  .datepicker-label{
+    font-size: 14px;
+    margin-left: 4px;
+  }
+  &-table{
+    padding: 6px;
+    .today{
+      color: #000;
+    }
+  }
+  &-btn{
+    color: #FFF;
+    &:hover{
+      color: #FFF;
+    }
+  }
+  &-input{
+    border: 1px solid $color-main;
+    border-radius: 10px;
+    box-shadow: $shadow-small;
+    overflow: hidden;
+    &:hover{
+      border: 1px solid $color-main-dark;
+    }
+  }
+  &-calendar{
+    padding: 0;
+    overflow: hidden;
+    height: auto;
+    &-content{
+      height: 100%;
+    }
+    &-header{
+      line-height: 26px;
+      box-shadow: $shadow-big;
+      background: $gradient;
+      overflow: hidden;
+      border-radius: 10px 10px 0 0 ;
+      padding: 4px;
+    }
+  }
+  &-datepicker{
+    width: 150px;
+    position: relative;
+    overflow: hidden;
+    &-main{
+      color: $color-main-dark;
+      font-family: "Montserrat";
+      border-radius: 10px; 
+      border: none;
+    }
+  }
+  &-wrapper{
+  }
+  &-icon-calendar{
+    background: $gradient;
+    color:#FFF;
+    padding: 8px;
+    right: 0px;
+    font-size: 18px;
+    border-radius: 0 10px 10px 0 ;
+  }
 }
-
 
 .search {
   &-wrapper {
-
+    // max-height: 100%;
     margin: 30px;
   }
   &__title {
@@ -454,34 +577,15 @@ export default {
     background: $gradient-w;
     &__inputs {
       display: flex;
-      justify-content: space-evenly;
-      align-items: center;
+      align-items: flex-end;
       padding: 20px;
+      margin-right: 40px;
     }
-    &__input {
-      position: relative;
-      overflow: hidden;
-      border-radius: 10px;
-      box-shadow: $shadow-small;
-      align-items: center;
-      margin-right: 20px;  
-      input{
-        height: 30px;
-        border: 1px solid transparent;
-        border-radius: 10px;
-        &:hover{
-          border: 1px solid $color-main;
-        }
-      }
-      i {
-        position: absolute;
-        right: 0;
-        padding: 8px;
-        background: $gradient;
-        color: #fff;
-        font-size: 0.875rem;
-      }
+    &__input{
+      display: flex;
+      flex-direction: column;
     }
+    
     &__arrow {
       max-height: 20px;
       margin-right: 20px;
@@ -490,21 +594,7 @@ export default {
         height: 100%;
       }
     }
-    &__select {
-      margin-left: 30px;
-      overflow: hidden;
-      border-radius: 10px;
-      box-shadow: $shadow-small;
-      align-items: center;
-      color: $color-main-dark;
-      height: 100%;
-      width: 113px;
-      height: 30px;
-    }
-    &__option {
-      color: $text-grey;
-      padding: 10px;
-    }
+
   }
   &-cloud {
     margin-top: 20px;
@@ -521,11 +611,13 @@ export default {
     border-radius: 10px;
     overflow: hidden;
     background: $gradient-w;
-    overflow-y: scroll;
   }
   &-buttons {
     margin: 10px;
     text-align: center;
   }
 }
+
+
+
 </style>

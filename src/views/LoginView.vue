627a647955d7b8d2b646c1a0f40c2edcab2f5ef5
Ann <ann.afanaseva777@gmail.com>
2022-06-03 17:58:20 +0300

    Vuelidate

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -11,13 +11,13 @@
       <div class="form">
         <div class="form-title">Авторизация</div>
 
-        <form class="form-wrapper">
+        <form class="form-wrapper" @submit.prevent="submitForm()">
           <div class="input-wrapper">
             <input
               placeholder=" "
               class="input input-withIcon"
-              v-model="user.login"
-              required
+              v-model.trim="login"
+              :class="{ invalid: v$.login.$error }"
             />
             <label class="input-label">Логин</label>
 
@@ -26,14 +26,17 @@
               class="input-img"
               src="@/assets/img/login-icon.svg"
             />
+            <div v-if="v$.password.$error" class="error-tooltip">
+              <p>Введите логин</p>
+            </div>
           </div>
 
           <div class="input-wrapper">
             <input
               placeholder=" "
               class="input input-withIcon"
-              v-model="user.password"
-              required
+              v-model.trim="password"
+              :class="{ invalid: v$.password.$error }"
             />
             <label class="input-label">Пароль</label>
 
@@ -42,11 +45,19 @@
               class="input-img"
               src="@/assets/img/lock-icon.svg"
             />
+            <div v-if="v$.password.$error" class="error-tooltip">
+              <p>Введите пароль</p>
+            </div>
           </div>
 
-          <router-link to="/main">
-            <button class="button button-g form-wrapper__item">Войти</button>
-          </router-link>
+          <!-- <router-link to="/main"> -->
+          <button
+            class="button button-g form-wrapper__item"
+            @click="submitForm()"
+          >
+            Войти
+          </button>
+          <!-- </router-link> -->
           <router-link to="/registration">
             <button class="button button-white form-wrapper__item">
               Регистрация
@@ -60,19 +71,41 @@
 
 <script>
 import { mapActions } from "vuex";
+import { reactive } from "@vue/composition-api";
+import useVuelidate from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
+
 export default {
   data() {
     return {
-      user: {
-        login: "",
-        password: "",
-      },
+      login: "",
+      password: "",
     };
+  },
+  setup() {
+    const state = reactive({
+      login: "",
+      password: "",
+    });
+    const rules = {
+      login: { required },
+      password: { required },
+    };
+
+    const v$ = useVuelidate(rules, state);
+
+    return { state, v$ };
   },
   methods: {
     ...mapActions("users", {
       authorize: "authorizeUser",
     }),
+    async submitForm() {
+      const isFormCorrect = await this.v$.$validate();
+      // you can show some extra alert to the user or just leave the each field to show it's `$errors`.
+      if (!isFormCorrect) return;
+      // actually submit form
+    },
   },
 };
 </script>
@@ -130,6 +163,41 @@ export default {
         margin: 10px auto;
         font-size: 16px;
       }
+    }
+  }
+}
+.invalid {
+  border: 1px solid $color-red;
+  transition: all 1s ease-out;
+  color: $color-red;
+  &:focus ~ .input-label,
+  &:not(:placeholder-shown) ~ label {
+    color: $color-red;
+  }
+}
+.error {
+  &-tooltip {
+    position: absolute;
+    right: -240px;
+    top: 50%;
+    transform: translate(0, -50%);
+    transition: all 2s ease-out;
+
+    display: flex;
+    align-items: center;
+
+    height: 49px;
+    width: 240px;
+    background: linear-gradient(
+      to right,
+      rgb(235, 96, 96, 0.6),
+      rgb(141, 70, 70, 0.6)
+    );
+    color: #fff;
+    font-size: 14px;
+    border-radius: 10px;
+    p {
+      margin-left: 8px;
     }
   }
 }
